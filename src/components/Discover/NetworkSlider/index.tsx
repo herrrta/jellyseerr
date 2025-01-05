5e385cded2f5c1627d63bf39d09ae52425931c04
herrrta <73949927+herrrta@users.noreply.github.com>
2025-01-05 11:20:39 -0500

    Keep predefined networks/studios from jellyseerr

---
+++ src/components/Discover/NetworkSlider/index.tsx
@@ -0,0 +1,142 @@
+export interface Network {
+  name: string;
+  image: string;
+  id: number;
+}
+
+export const COMPANY_LOGO_IMAGE_FILTER = 'w780_filter(duotone,ffffff,bababa)';
+
+export const networks: Network[] = [
+  {
+    name: 'Netflix',
+    image:
+      '/wwemzKWzjKYJFfCeiB57q3r4Bcm.png',
+    id: 213,
+  },
+  {
+    name: 'Disney+',
+    image:
+      '/gJ8VX6JSu3ciXHuC2dDGAo2lvwM.png',
+    id: 2739,
+  },
+  {
+    name: 'Prime Video',
+    image:
+      '/ifhbNuuVnlwYy5oXA5VIb2YR8AZ.png',
+    id: 1024,
+  },
+  {
+    name: 'Apple TV+',
+    image:
+      '/4KAy34EHvRM25Ih8wb82AuGU7zJ.png',
+    id: 2552,
+  },
+  {
+    name: 'Hulu',
+    image:
+      '/pqUTCleNUiTLAVlelGxUgWn1ELh.png',
+    id: 453,
+  },
+  {
+    name: 'HBO',
+    image:
+      '/tuomPhY2UtuPTqqFnKMVHvSb724.png',
+    id: 49,
+  },
+  {
+    name: 'Discovery+',
+    image:
+      '/1D1bS3Dyw4ScYnFWTlBOvJXC3nb.png',
+    id: 4353,
+  },
+  {
+    name: 'ABC',
+    image:
+      '/ndAvF4JLsliGreX87jAc9GdjmJY.png',
+    id: 2,
+  },
+  {
+    name: 'FOX',
+    image:
+      '/1DSpHrWyOORkL9N2QHX7Adt31mQ.png',
+    id: 19,
+  },
+  {
+    name: 'Cinemax',
+    image:
+      '/6mSHSquNpfLgDdv6VnOOvC5Uz2h.png',
+    id: 359,
+  },
+  {
+    name: 'AMC',
+    image:
+      '/pmvRmATOCaDykE6JrVoeYxlFHw3.png',
+    id: 174,
+  },
+  {
+    name: 'Showtime',
+    image:
+      '/Allse9kbjiP6ExaQrnSpIhkurEi.png',
+    id: 67,
+  },
+  {
+    name: 'Starz',
+    image:
+      '/8GJjw3HHsAJYwIWKIPBPfqMxlEa.png',
+    id: 318,
+  },
+  {
+    name: 'The CW',
+    image:
+      '/ge9hzeaU7nMtQ4PjkFlc68dGAJ9.png',
+    id: 71,
+  },
+  {
+    name: 'NBC',
+    image:
+      '/o3OedEP0f9mfZr33jz2BfXOUK5.png',
+    id: 6,
+  },
+  {
+    name: 'CBS',
+    image:
+      '/nm8d7P7MJNiBLdgIzUK0gkuEA4r.png',
+    id: 16,
+  },
+  {
+    name: 'Paramount+',
+    image:
+      '/fi83B1oztoS47xxcemFdPMhIzK.png',
+    id: 4330,
+  },
+  {
+    name: 'BBC One',
+    image:
+      '/mVn7xESaTNmjBUyUtGNvDQd3CT1.png',
+    id: 4,
+  },
+  {
+    name: 'Cartoon Network',
+    image:
+      '/c5OC6oVCg6QP4eqzW6XIq17CQjI.png',
+    id: 56,
+  },
+  {
+    name: 'Adult Swim',
+    image:
+      '/9AKyspxVzywuaMuZ1Bvilu8sXly.png',
+    id: 80,
+  },
+  {
+    name: 'Nickelodeon',
+    image:
+      '/ikZXxg6GnwpzqiZbRPhJGaZapqB.png',
+    id: 13,
+  },
+  {
+    name: 'Peacock',
+    image:
+      '/gIAcGTjKKr0KOHL5s4O36roJ8p7.png',
+    id: 3353,
+  },
+];
---
+++ src/components/Discover/StudioSlider/index.tsx
@@ -0,0 +1,68 @@
+export interface Studio {
+  name: string;
+  image: string;
+  id: number;
+}
+
+export const studios: Studio[] = [
+  {
+    name: 'Disney',
+    image:
+      '/wdrCwmRnLFJhEoH8GSfymY85KHT.png',
+    id: 2,
+  },
+  {
+    name: '20th Century Studios',
+    image:
+      '/h0rjX5vjW5r8yEnUBStFarjcLT4.png',
+    id: 127928,
+  },
+  {
+    name: 'Sony Pictures',
+    image:
+      '/GagSvqWlyPdkFHMfQ3pNq6ix9P.png',
+    id: 34,
+  },
+  {
+    name: 'Warner Bros. Pictures',
+    image:
+      '/ky0xOc5OrhzkZ1N6KyUxacfQsCk.png',
+    id: 174,
+  },
+  {
+    name: 'Universal',
+    image:
+      '/8lvHyhjr8oUKOOy2dKXoALWKdp0.png',
+    id: 33,
+  },
+  {
+    name: 'Paramount',
+    image:
+      '/fycMZt242LVjagMByZOLUGbCvv3.png',
+    id: 4,
+  },
+  {
+    name: 'Pixar',
+    image:
+      '/1TjvGVDMYsj6JBxOAkUHpPEwLf7.png',
+    id: 3,
+  },
+  {
+    name: 'Dreamworks',
+    image:
+      '/kP7t6RwGz2AvvTkvnI1uteEwHet.png',
+    id: 521,
+  },
+  {
+    name: 'Marvel Studios',
+    image:
+      '/hUzeosd33nzE5MCNsZxCGEKTXaQ.png',
+    id: 420,
+  },
+  {
+    name: 'DC',
+    image:
+      '/2Tc1P3Ac8M479naPp1kYT3izLS5.png',
+    id: 9993,
+  },
+];
