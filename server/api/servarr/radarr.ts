4401b16414af604a7372dacac326c38b18ad8555
herrrta <73949927+herrrta@users.noreply.github.com>
2025-01-07 18:47:33 -0500

    add sevarrs

---
+++ server/api/servarr/radarr.ts
@@ -0,0 +1,28 @@
+export interface RadarrMovieOptions {
+  title: string;
+  qualityProfileId: number;
+  minimumAvailability: string;
+  tags: number[];
+  profileId: number;
+  year: number;
+  rootFolderPath: string;
+  tmdbId: number;
+  monitored?: boolean;
+  searchNow?: boolean;
+}
+
+export interface RadarrMovie {
+  id: number;
+  title: string;
+  isAvailable: boolean;
+  monitored: boolean;
+  tmdbId: number;
+  imdbId: string;
+  titleSlug: string;
+  folderName: string;
+  path: string;
+  profileId: number;
+  qualityProfileId: number;
+  added: string;
+  hasFile: boolean;
+}
